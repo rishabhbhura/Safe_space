1af1cb99220cd4186623195bd0d658af2118f0c5
rishabhbhura <53896513+rishabhbhura@users.noreply.github.com>
2020-12-02 07:54:48 +0530

    commit

--- safespace/settings.py
+++ safespace/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = "/home/safespace12/Safe_space/static"
 
 LOGIN_REDIRECT_URL = "test"
 LOGOUT_REDIRECT_URL = "thanks"
